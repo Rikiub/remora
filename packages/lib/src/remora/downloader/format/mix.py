d5670eadafce632520f5bc7f783e7553df01187d
Rikiub <rikiub.dev@gmail.com>
2026-02-10 11:24:16 -0400

    chore(lib): fix import location

--- packages/lib/src/remora/downloader/format/mix.py
+++ packages/lib/src/remora/downloader/format/mix.py
@@ -1,11 +1,11 @@
 from pathlib import Path
-
+from typing_extensions import override
 
 from remora.downloader.format.base import DEFAULT_RETRIES, BaseFormatDownloader
 from remora.downloader.format.httpx import HttpxFormatDownloader
 from remora.downloader.format.ydl import YDLFormatDownloader
 from remora.exceptions import DownloadError
-from remora.models.format.types import Format, override
+from remora.models.format.types import Format
 from remora.models.progress.format import FormatDownloadCallback
 from remora.types import StrPath
 
