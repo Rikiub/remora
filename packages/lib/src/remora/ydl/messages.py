4dc370659884f0f085fc22f9529e2ff244f9d594
Rikiub <rikiub.dev@gmail.com>
2026-02-16 17:51:41 -0400

    chore(lib): simply ydl exceptions

--- packages/lib/src/remora/ydl/messages.py
+++ packages/lib/src/remora/ydl/messages.py
@@ -10,14 +10,11 @@ class ExceptMsg(NamedTuple):
 MESSAGES: list[ExceptMsg] = [
     ExceptMsg(
         matchs=["HTTP Error 403"],
-        text=lambda v: (
-            v
-            + " : You may have exceeded the page request limit, received an IP block, among others. Please try again later."
-        ),
+        text="HTTP Error 403: Forbidden: You may have exceeded the page request limit or received an IP block",
     ),
     ExceptMsg(
         matchs=["Read timed out"],
-        text="Read timed out.",
+        text="Read timed out",
     ),
     ExceptMsg(
         matchs=["Unable to download webpage"],
@@ -27,7 +24,7 @@ MESSAGES: list[ExceptMsg] = [
     ),
     ExceptMsg(
         matchs=["is not a valid URL"],
-        text=lambda v: v.split()[1] + " is not a valid URL.",
+        text=lambda v: v.split()[1] + " is not a valid URL",
     ),
     ExceptMsg(
         matchs=["Unsupported URL"],
@@ -35,31 +32,31 @@ MESSAGES: list[ExceptMsg] = [
     ),
     ExceptMsg(
         matchs=["Unable to extract webpage video data"],
-        text="Unable to extract webpage video data.",
+        text="Unable to extract webpage video data",
     ),
     ExceptMsg(
         matchs=["Private video. Sign in if you've been granted access to this video."],
-        text="Private video, unable to download.",
+        text="Private video, unable to download",
     ),
     ExceptMsg(
         matchs=["Unable to rename file"],
-        text="Unable to rename file.",
+        text="Unable to rename file",
     ),
     ExceptMsg(
         matchs=["ffmpeg not found"],
-        text="Postprocessing failed. FFmpeg executable not founded.",
+        text="Processing failed. FFmpeg executable not founded",
     ),
     ExceptMsg(
         matchs=["No video formats found!"],
-        text="No formats founded.",
+        text="No formats founded",
     ),
     ExceptMsg(
         matchs=["Unable to download", "Got error"],
-        text="Unable to download.",
+        text="Unable to download",
     ),
     ExceptMsg(
         matchs=["is only available for registered users"],
-        text="Only available for registered users.",
+        text="Only available for registered users",
     ),
 ]
 
@@ -67,10 +64,8 @@ MESSAGES: list[ExceptMsg] = [
 def format_except_message(exception: Exception) -> str:
     """Get a user friendly message of a YT-DLP message exception."""
 
-    message: str = str(exception)
-
-    if message.startswith("ERROR: "):
-        message = message.strip("ERROR: ")
+    message = str(exception)
+    message = message.removeprefix("ERROR: ")
 
     for item in MESSAGES:
         if any(s in message for s in item.matchs):
