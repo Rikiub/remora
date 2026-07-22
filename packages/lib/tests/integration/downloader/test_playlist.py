36758426382e6f716c4d0518f6ad79c2a4cd9a31
Rikiub <rikiub.dev@gmail.com>
2026-07-22 16:52:43 -0400

    chore(lib/tests): Move wrong soundcloud playlist from url -> playlist

--- packages/lib/tests/integration/downloader/test_playlist.py
+++ packages/lib/tests/integration/downloader/test_playlist.py
@@ -3,3 +3,9 @@ async def test_youtube_music(download):
     await download(
         "https://music.youtube.com/playlist?list=OLAK5uy_lRrAuEy29zo5mtAH465aEtvmRfakErDoI"
     )
+
+
+async def test_soundcloud(download):
+    await download(
+        "https://soundcloud.com/playlist/sets/sound-of-berlin-01-qs1-x-synth"
+    )
--- packages/lib/tests/integration/downloader/test_url.py
+++ packages/lib/tests/integration/downloader/test_url.py
@@ -10,15 +10,9 @@ async def test_tiktok(download):
     await download("https://www.tiktok.com/@livewallpaper77/video/7410777368064806149")
 
 
-async def test_netease_music(download):
+async def test_netease_song(download):
     await download("http://music.163.com/#/song?id=421563082")
 
 
 async def test_bandcamp(download):
     await download("https://gourmetdeluxxx.bandcamp.com/track/nocturnal-hooli")
-
-
-async def test_soundcloud_playlist(download):
-    await download(
-        "https://soundcloud.com/playlist/sets/sound-of-berlin-01-qs1-x-synth"
-    )
