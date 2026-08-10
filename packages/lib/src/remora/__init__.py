b2807b098f5e0cf33bec1252622073dab358793f
Rikiub% <rikiub.dev@gmail.com>
2026-08-10 02:22:29 -0400

    fix(lib): ImportFrom error when use __all__

--- packages/lib/src/remora/__init__.py
+++ packages/lib/src/remora/__init__.py
@@ -1,5 +1,6 @@
 """Remora: data extractor and downloader."""
 
+import ast as _ast
 from typing import TYPE_CHECKING as _TYPE_CHECKING
 
 import lazy_imports as _lazy_imports
@@ -11,7 +12,11 @@ if _TYPE_CHECKING:
 _lazy_imports.load(
     module=_lazy_imports.LazyModule(
         *_lazy_imports.as_package(__file__),
-        _lazy_imports.module_source(".__init__imports", __name__),
+        *(
+            node
+            for node in _ast.parse(_lazy_imports.module_source(".__init__imports", __name__)).body
+            if isinstance(node, _ast.ImportFrom)
+        ),
         name=__name__,
         doc=__doc__,
     )
