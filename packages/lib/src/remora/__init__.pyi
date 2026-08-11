48d2107a258d5359143af07edfea4b94dcb0d2ac
Rikiub% <rikiub.dev@gmail.com>
2026-08-10 22:03:40 -0400

    refactor(lib)!: Remove majority of types from public API

--- packages/lib/src/remora/__init__.pyi
+++ packages/lib/src/remora/__init__.pyi
@@ -4,52 +4,23 @@ from ._internal.api import Remora
 from ._internal.extractor import MediaExtractor
 
 # --- TYPES ---
-from .models.container import (
-    AudioExtension,
-    AudioExtensionType,
-    ExtensionType,
-    FormatType,
-    VideoExtension,
-    VideoExtensionType,
-)
 from .models.download_options import DownloadOptions
-
-# Events
-from .models.event import (
-    BatchEvent,
-    BatchStreamEvent,
-    MediaEvent,
-    PlaylistEvent,
-    StreamEvent,
-)
 from .models.media import LazyMedia, LazyPlaylist, Media, Playlist, SearchList
-from .models.protocol import Protocol, ProtocolType
-from .models.stream import AudioStream, Stream, VideoStream
+from .models.stream import AudioStream, MuxedStream, Stream, StreamList, VideoStream
 
 __all__ = [
-    "AudioExtension",
-    "AudioExtensionType",
     "AudioStream",
-    "BatchEvent",
-    "BatchStreamEvent",
     "DownloadOptions",
-    "ExtensionType",
-    "FormatType",
     "LazyMedia",
     "LazyPlaylist",
     "Media",
-    "MediaEvent",
     "MediaExtractor",
+    "MuxedStream",
     "Playlist",
-    "PlaylistEvent",
-    "Protocol",
-    "ProtocolType",
     "Remora",
     "SearchList",
     "Stream",
-    "StreamEvent",
-    "VideoExtension",
-    "VideoExtensionType",
+    "StreamList",
     "VideoStream",
     "exceptions",
     "logs",
