c604987d493dc55a7e5339713da5d326912af424
Rikiub% <rikiub.dev@gmail.com>
2026-08-27 22:12:16 -0400

    test(lib): Remove unused test

--- packages/lib/tests/unit/models/enum/test_extensions.py
+++
@@ -1,8 +0,0 @@
-def test_audio_extension_match_enum():
-    """`AudioExtensionStr` must stay in sync with the `AudioExtension` members."""
-    # assert set(get_args(_AudioContainerLiteral)) == {e for e in AudioContainer}
-
-
-def test_video_extension_match_enum():
-    """`VideoExtensionStr` must stay in sync with the `VideoExtension` members."""
-    # assert set(get_args(_VideoContainerLiteral)) == {e for e in VideoContainer}
