86f600384ea3a399e32206573ce58d8a09bde893
Rikiub% <rikiub.dev@gmail.com>
2026-09-23 13:43:06 -0400

    refactor(lib)!: Remove MediaExtractor from public API

--- packages/remora/src/remora/__init__.pyi
+++ packages/remora/src/remora/__init__.pyi
@@ -1,11 +1,9 @@
 from . import constants, downloader, exceptions, ffmpeg, logs, models, path, template
 from .api import Remora
-from .extractor import MediaExtractor
 from .models.cookies import Cookie, Cookies
 from .models.media import LazyMedia, LazyPlaylist, Media, Playlist, Search
 from .models.options import DownloadOptions, NetworkOptions
 from .processor import MediaProcessor
-from .session import Session
 
 __all__ = [
     "Cookie",
@@ -14,13 +12,11 @@ __all__ = [
     "LazyMedia",
     "LazyPlaylist",
     "Media",
-    "MediaExtractor",
     "MediaProcessor",
     "NetworkOptions",
     "Playlist",
     "Remora",
     "Search",
-    "Session",
     "constants",
     "downloader",
     "exceptions",
